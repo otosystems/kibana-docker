63631fa802bf812b20e77a5b35fd283f65a749ea
Grischa Meyer <grischa@gmail.com>
2019-03-26 13:32:19 +0100

    fixup! OTO-541: oto base image

--- make-oto.sh
+++ make-oto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pip install awscli
 eval $(aws ecr get-login --no-include-email --region us-east-1)
+docker pull $BASE_IMAGE
 make
 export REPO=kibana-docker
 export REGISTRY=$AWS_ACCOUNT_NUMBER.dkr.ecr.us-east-1.amazonaws.com
